7ddbc9d05c7a1501c6629abd4599ee1b5737bc84
huzaifa <hikothari128@gmail.com>
2020-09-19 21:30:47 -0700

    changed VPC CIDR, subnet conf, all values read from config

--- CDK/lib/cdk-stack.ts
+++ CDK/lib/cdk-stack.ts
@@ -1,33 +1,54 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 
+import config from '../config.json';
+
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const publicSNs = [
+      {
+        subnetType: ec2.SubnetType.PUBLIC,
+        name: 'public1',
+        cidrMask:config.SUBNET_CIDR_MASK,      
+      },
+      {
+        subnetType: ec2.SubnetType.PUBLIC,
+        name: 'public2',
+        cidrMask:config.SUBNET_CIDR_MASK,      
+      },
+      {
+        subnetType: ec2.SubnetType.PUBLIC,
+        name: 'public3',
+        cidrMask:config.SUBNET_CIDR_MASK,      
+      }
+    ]
+    
+    const isolatedSNs = [
+      {
+        subnetType: ec2.SubnetType.ISOLATED,
+        name: 'isolated1',
+        cidrMask:config.SUBNET_CIDR_MASK,      
+      },
+      {
+        subnetType: ec2.SubnetType.ISOLATED,
+        name: 'isolated2',
+        cidrMask:config.SUBNET_CIDR_MASK,      
+      },
+      {
+        subnetType: ec2.SubnetType.ISOLATED,
+        name: 'isolated3',
+        cidrMask:config.SUBNET_CIDR_MASK,      
+      }
+    ]
     const vpc = new ec2.Vpc(this, "IAC-VPC", {
-      cidr: "10.0.0.0/21",
+      cidr: [config.VPC_IP, config.VPC_CIDR_MASK].join("/"),
       maxAzs:1,
       natGateways:0,
-      subnetConfiguration: [
-        {
-          subnetType: ec2.SubnetType.PUBLIC,
-          name: 'publicSN',
-          cidrMask:24,
-        },
-        {
-          subnetType:ec2.SubnetType.ISOLATED,
-          name: 'isolatedSN1',
-          cidrMask:24,          
-          
-        },
-        {
-          subnetType:ec2.SubnetType.ISOLATED,
-          name: 'isolatedSN2',
-          cidrMask:24,
-        }
-      ]
+      subnetConfiguration: [...publicSNs, ...isolatedSNs]
     });
   }
 }
