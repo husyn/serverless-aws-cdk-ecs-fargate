48fc0c5485c0d4f8c21b41b701f0eb28be5c0667
saifsms91 <saifsms91@rediffmail.com>
2020-10-12 12:38:46 +0800

    ecs created

--- CDK/lib/cdk-stack.ts
+++ CDK/lib/cdk-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecr from '@aws-cdk/aws-ecr';
+import * as ecs from "@aws-cdk/aws-ecs";
+
 
 import config from '../config.json';
 
@@ -54,4 +56,22 @@ export class CdkStack extends cdk.Stack {
 
     const ecr_repo = new ecr.Repository(this, config.project_name);
   }
+
+  const vpc = new ec2.Vpc(this, "MyVpc", {
+        maxAzs: 3 // Default is all AZs in region
+      });
+
+      const cluster = new ecs.Cluster(this, "MyCluster", {
+        vpc: vpc
+      });
+
+      // Create a load-balanced Fargate service and make it public
+      new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+        cluster: cluster, // Required
+        cpu: 512, // Default is 256
+        desiredCount: 6, // Default is 1
+        taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+        memoryLimitMiB: 2048, // Default is 512
+        publicLoadBalancer: true // Default is false
+      });
 }
