83797c28b7b2ac47f58ed3d60ae1c9899da144b7
Hussain Mansoor <hussain_raj1@yahoo.com>
2021-04-08 20:44:00 +0800

    Updates the format of the project

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # serverless-aws-cdk-iac
 Infrastructure As Code (IaC) using AWS CDK with Typescript to create a complete serverless application
 
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+ * `npm run build`   compile typescript to js
+ * `npm run watch`   watch for changes and compile
+ * `npm run test`    perform the jest unit tests
+ * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `cdk diff`        compare deployed stack with current state
+ * `cdk synth`       emits the synthesized CloudFormation template
+
+
 ## Services to create
 
 - Subnets (3 subnets)
--- cdk-stack.ts
+++ cdk-stack.ts
@@ -4,16 +4,24 @@ import * as ecr from '@aws-cdk/aws-ecr';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as alb from '@aws-cdk/aws-elasticloadbalancingv2';
 
-import config from '../config.json';
+const config = require('config');
+
 import { Protocol } from '@aws-cdk/aws-ec2';
 import { LoadBalancingProtocol } from '@aws-cdk/aws-elasticloadbalancing';
+import { Aws, Duration } from '@aws-cdk/core';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //validitity tests
+    if (config.ENVIRONMENT != environments.DEV 
+      && config.ENVIRONMENT != environments.STG 
+      && config.ENVIRONMENT != environments.PRD) {
+        throw new Error("Invalid environment in config");
+      }
 
+    //networking
     const publicSubnets = [
       {
         subnetType: ec2.SubnetType.PUBLIC,
@@ -49,6 +57,7 @@ export class CdkStack extends cdk.Stack {
         cidrMask:config.SUBNET_CIDR_MASK,      
       }
     ]
+
     const vpc = new ec2.Vpc(this, this.get_logical_prodenv_name('vpc'), {
       cidr: [config.VPC_IP, config.VPC_CIDR_MASK].join("/"),
       maxAzs:3,
@@ -56,17 +65,34 @@ export class CdkStack extends cdk.Stack {
       subnetConfiguration: [...publicSubnets, ...privateSubnets]
     });
 
+    //LB
     const applb = new alb.ApplicationLoadBalancer(this, 'LB', {
-      vpc,
+      vpc: vpc,
       internetFacing: true,
       loadBalancerName: this.get_logical_prodenv_name('lb')
     });
 
     const listener = applb.addListener('Listener', {
-      port: 8080      
+      port: 8080,
+      protocol: alb.ApplicationProtocol.HTTP
     });
 
+    let deregistration_delay = 10;
+    if (config.ENVIRONMENT == environments.PRD) {
+      deregistration_delay = 300;
+    }
 
+    const target_group = new alb.ApplicationTargetGroup(this, this.get_logical_prodenv_name('targetgroup'), {
+      // deregistrationDelay: deregistration_delay,
+      port: 8080,
+      protocol: alb.ApplicationProtocol.HTTP,
+      targetGroupName: this.get_logical_prodenv_name('targetgroup'),
+      vpc: vpc,
+      targetType: alb.TargetType.IP
+      
+    });
+  
+    //ECS Cluster
     const ecr_repo = new ecr.Repository(this, config.PROJECT_NAME);
 
     const cluster = new ecs.Cluster(this, this.get_logical_prodenv_name('cluster'), {
@@ -74,24 +100,27 @@ export class CdkStack extends cdk.Stack {
       clusterName: this.get_logical_prodenv_name('cluster')
     });
 
-    
-
     const td = new ecs.TaskDefinition(this, this.get_logical_prodenv_name('taskdefinition'), {
       compatibility: ecs.Compatibility.FARGATE,
       cpu: '512',
-      memoryMiB: '1024',
-      executionRole: 
-
+      memoryMiB: '1024'      
+    });
+    td.addContainer('container', {
+      image: ecr_repo.repositoryUri
     });
 
     const service = new ecs.FargateService(this, this.get_logical_prodenv_name('service'), {
       cluster: cluster,
       assignPublicIp: false,
       desiredCount: 1,
-      taskDefinition: ''
+      taskDefinition: td,
+      serviceName: this.get_logical_prodenv_name('service'),
+      vpcSubnets: ec2.Vpc.fromVpcAttributes(this, "vpc", vpc)
     });
 
   }
+
+  //utility
 
   get_logical_prodenv_name(resource_type:string):string {
     
@@ -102,5 +131,10 @@ export class CdkStack extends cdk.Stack {
     
     return val;
   }
-
 }
+
+enum environments {
+  DEV = 'dev',
+  STG = 'stg',
+  PRD = 'prod'
+};
